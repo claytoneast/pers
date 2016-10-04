e6faa560645c35852868888f6544b991954fba1d
claytoneast <claytoneast@gmail.com>
2016-10-04 11:55:35 -0700

    adds fixed-size spinner markup

---
+++ main.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  let spinner = document.getElementById('spinner-container')
+  let style = window.getComputedStyle(spinner)
+  var matrix = new WebKitCSSMatrix(style.webkitTransform);
+  debugger
+
+}
