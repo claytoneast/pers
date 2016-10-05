4c24db2f6b526950995668e8298cf4d9a8c45c77
claytoneast <claytoneast@gmail.com>
2016-10-04 18:54:09 -0700

    adds functional spin movement on fixed sized spinner

--- main.js
+++ main.js
@@ -1,7 +1,50 @@
-window.onload = function () {
-  let spinner = document.getElementById('spinner-container')
-  let style = window.getComputedStyle(spinner)
-  var matrix = new WebKitCSSMatrix(style.webkitTransform);
-  debugger
+let spinner
+let spinnerLocation = 1 // spinner1 in view
+let currentRotation = 0
 
+window.onload = function () {
+  initializePage()
+  spinner = document.getElementById('spinner-container')
+}
+
+function initializePage () {
+  let uiControls = document.getElementsByClassName('ui-controls-item')
+  Array.from(uiControls).forEach(function (elem) {
+    elem.addEventListener('click', changeTiles, false)
+  })
+}
+
+function changeTiles (e) {
+  let targetTileId = e.target.dataset.spinnertarget
+  if (spinnerLocation === 1) {
+    if (targetTileId === 'spinner2') {
+      spin('clockwise')
+      spinnerLocation = 2
+    } else if (targetTileId === 'spinner3') {
+      spin('counterclockwise')
+      spinnerLocation = 3
+    }
+  } else if (spinnerLocation === 2) {
+    if (targetTileId === 'spinner1') {
+      spin('counterclockwise')
+      spinnerLocation = 1
+    } else if (targetTileId === 'spinner3') {
+      spin('clockwise')
+      spinnerLocation = 3
+    }
+  } else if (spinnerLocation === 3) {
+    if (targetTileId === 'spinner1') {
+      spin('clockwise')
+      spinnerLocation = 1
+    } else if (targetTileId === 'spinner2') {
+      spin('counterclockwise')
+      spinnerLocation = 2
+    }
+  }
+}
+
+function spin (direction) {
+  let nextRotation = direction === 'clockwise' ? currentRotation + 120 : currentRotation - 120
+  spinner.style.transform = `rotate(${nextRotation}deg)`
+  currentRotation = nextRotation
 }
