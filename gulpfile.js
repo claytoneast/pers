af54b334e9f61ab26a486ed1d59df11f369956f1
claytoneast <claytoneast@gmail.com>
2016-10-10 16:20:49 -0700

    adds autoprefixer to watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ var gulp = require('gulp')
 var sass = require('gulp-sass')
 
 gulp.task('sass', function () {
-  return gulp.src('*.scss')
+  return gulp.src('styles.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(''))
 })
 
 gulp.task('watch', function () {
-  gulp.watch('*.scss', ['sass'])
+  gulp.watch('styles.scss', ['autoprefixer'])
 })
 
-gulp.task('autoprefixer', function () {
+gulp.task('autoprefixer', ['sass'], function () {
   var postcss = require('gulp-postcss')
   var sourcemaps = require('gulp-sourcemaps')
   var autoprefixer = require('autoprefixer')
